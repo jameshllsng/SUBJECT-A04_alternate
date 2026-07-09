ba942722b8976d4331128eded916c5f6a5f43039
Luís (James) Miguel <lmsandrincardoso@gmail.com>
2026-07-09 15:57:55 -0300

    code identation changes

--- Assets/Scripts/Player/PlayerLook.cs
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -9,24 +8,27 @@ public class PlayerLook : MonoBehaviour
     [SerializeField] private float mouseSensitivity = 0.2f;
     private float xRotation;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Start()
     {
         lookAction = InputSystem.actions.FindAction("Look");
         playerCamera = Camera.main.transform;
+
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    // Update is called once per frame
     private void Update()
     {
         Vector2 lookInput = lookAction.ReadValue<Vector2>();
+
         float mouseX = lookInput.x * mouseSensitivity;
         float mouseY = lookInput.y * mouseSensitivity;
+
         transform.Rotate(Vector3.up * mouseX);
+        
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
         playerCamera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,18 +1,15 @@
-using System;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private InputAction moveAction;
+    private InputAction jumpAction;
     private CharacterController controller;
     [SerializeField] private float moveSpeed = 5f;
-    private InputAction jumpAction;
-    private float verticalVelocity = 0f;
     [SerializeField] private float gravity = -15f;
     [SerializeField] private float jumpForce = 5.5f;
-
+    private float verticalVelocity;
 
     private void Awake()
     {
@@ -26,20 +23,25 @@ public class PlayerMovement : MonoBehaviour
     }
     
     private void Update()
+    {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+        Vector3 moveDirection = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        if (controller.isGrounded && verticalVelocity < 0f)
         {
-            Vector2 moveInput = moveAction.ReadValue<Vector2>();
-            Vector3 moveDirection = transform.right * moveInput.x + transform.forward * moveInput.y;
-            if (controller.isGrounded && verticalVelocity < 0f)
-            {
-                verticalVelocity = -2f;
-            }
-            if (jumpAction.WasPressedThisFrame() && controller.isGrounded)
-            {
-                verticalVelocity = jumpForce;
-            }
-            verticalVelocity += gravity * Time.deltaTime;
-            Vector3 finalMove = moveDirection * moveSpeed;
-            finalMove.y = verticalVelocity;
-            controller.Move(finalMove * Time.deltaTime);
+            verticalVelocity = -2f;
         }
+
+        if (jumpAction.WasPressedThisFrame() && controller.isGrounded)
+        {
+            verticalVelocity = jumpForce;
+        }
+        
+        verticalVelocity += gravity * Time.deltaTime;
+
+        Vector3 finalMove = moveDirection * moveSpeed;
+        finalMove.y = verticalVelocity;
+
+        controller.Move(finalMove * Time.deltaTime);
+    }
 }
